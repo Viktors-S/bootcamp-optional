e4aec6b320c1e403f96b3f5dbadeb1b79fa698b0
Viktors-S <viktorsseils21@gmail.com>
2023-08-31 13:42:54 +0300

    changes

--- main.tf
+++ main.tf
@@ -33,6 +33,15 @@ resource "aws_subnet" "public_subnet" {
     Name = "TVAPublicSubnet"
   }
 }
+resource "aws_subnet" "public_subnet2" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = "192.168.3.0/24"
+  availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "TVAPublicSubnet2"
+  }
+}
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.custom_vpc.id
@@ -43,6 +52,45 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+//auto scaling and launch group
+resource "aws_launch_configuration" "launch" {
+  name_prefix   = "launch-cfg"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_group" "ats" {
+  name                 = "autoscalingV"
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = var.min_size
+  max_size             = var.max_size
+  desired_capacity     = var.desired_capacity
+}
+
+
+//load balancer
+resource "aws_lb" "lb" {
+  name               = "TVAlb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_subnet.id,aws_subnet.public_subnet2.id]  # Replace with your subnet IDs
+}
+
+resource "aws_lb_target_group" "example" {
+  name     = "example-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.custom_vpc.id # Replace with your VPC ID
+}
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  default_action {
+    target_group_arn = aws_lb_target_group.example.arn
+    type             = "fixed-response"
+  }
+}
 
 
 
